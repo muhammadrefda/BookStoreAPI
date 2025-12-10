e978f89e6c39c2c58645ac651bab9fbe68efb671
muhammad_refda <muhammadrefda@gmail.com>
2025-12-10 07:33:49 +0700

    create scaffolding

---
+++ src/test/java/com/refda/bookstore/BookStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refda.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
